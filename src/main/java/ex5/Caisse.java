c246b2e730a28b2cbfeec49ef93255ec8bcddec7
elouanb7 <elouanb7@gmail.com>
2024-04-23 17:29:21 +0200

    Exercice 5

--- src/main/java/ex5/Caisse.java
+++ src/main/java/ex5/Caisse.java
@@ -6,53 +6,32 @@ import java.util.List;
 public class Caisse {
 
     private String nom;
+    private int capaciteMax;
     private List<Item> items;
 
-    /**
-     * Constructeur
-     *
-     * @param nom
-     */
-    public Caisse(String nom) {
-        super();
+    public Caisse(String nom, int capaciteMax) {
         this.nom = nom;
+        this.capaciteMax = capaciteMax;
         this.items = new ArrayList<>();
     }
 
-    /**
-     * Getter pour l'attribut nom
-     *
-     * @return the nom
-     */
     public String getNom() {
         return nom;
     }
 
-    /**
-     * Setter pour l'attribut nom
-     *
-     * @param nom the nom to set
-     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
-    /**
-     * Getter pour l'attribut items
-     *
-     * @return the items
-     */
     public List<Item> getItems() {
         return items;
     }
 
-    /**
-     * Setter pour l'attribut items
-     *
-     * @param items the items to set
-     */
     public void setItems(List<Item> items) {
         this.items = items;
     }
 
+    public boolean peutAjouter(Item item) {
+        return item.getPoids() <= capaciteMax;
+    }
 }
